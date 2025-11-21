344205646aba2018da042f0a69a004e93c9c346a
Sharada1415 <ramyar.alpha@gmail.com>
2025-11-21 14:09:12 +0530

    adding function

--- learning.py
+++ learning.py
@@ -1,5 +1,8 @@
-for i in range(1, 10):
-    if i%2==0:
-        print("even")
-    else:
-        print("odd")
+def even_odd(start, end):
+    for i in range(start, end):
+        if i%2==0:
+            print("even")
+        else:
+            print("odd")
+
+even_odd(10, 20)
