704e7918d582db468f89133c08e2e75c3e8c16fe
Sharada1415 <ramyar.alpha@gmail.com>
2025-11-21 14:23:21 +0530

    added prime_numbers attribute to the Numbers class

--- learning.py
+++ learning.py
@@ -6,6 +6,16 @@ class Numbers:
                 print("even")
             else:
                 print("odd")
-    
+
+    def prime_numbers(self):
+        for i in range(2, 50):
+            for j in range(2, i):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+
+
 num = Numbers()
 num.even_odd(10, 20)
+num.prime_numbers()
