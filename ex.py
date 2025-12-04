6663dca8694b7475a472312b678ebf88a8332105
Sharada1415 <ramyar.alpha@gmail.com>
2025-12-04 14:51:11 +0530

    git commands

--- ex.py
+++ ex.py
@@ -37,12 +37,97 @@
 #             else:
 #                 print("odd")
 
-for i in range(2, 50):
-    for j in range(2, i):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+# for i in range(2, 50):
+#     for j in range(2, i):
+#         if i % j == 0:
+#             break
+#     else:
+#         print(i)
+
+
+
+
+
+
+# import pytest
+# from selenium import webdriver
+#
+# @pytest.fixture()
+# def setup():
+#     driver = webdriver.Chrome()
+#     driver.get('')
+#     yield driver
+#
+# ## setup --> driver
+#
+# def test_case1(setup):
+#     setup.find_element('xpath', 'value')
+#
+# def test_case2(setup):
+#     setup.find_element('xpath', 'value')
+
+##########################################################################
+
+'''
+*   git --version       --> Check the git version
+*   To add username     --> git config --global user.name username
+*   To add email        --> git config --global user.email email_id
+
+To check the username and password
+*   git config user.name    --> Display username
+*   git config user.email   --> Display email
+
+*   To change the cmd prompt path --> cd <path of the project>
+
+*   Initialize the repository   --> git init
+
+*   Create a new directory  --> mkdir dirname
+*   To remove the directory --> rmdir dirname
+
+*   To check the status of the git  -->     git status
+
+*   To add files to the staging area    --> git add filename
+    To add all the files                --> git add .
+
+*   To remove the files from the staging area   --> git rm --cached filename
+
+*   To commit   --> git commit -m "descriptive message"
+
+*   To check all the commits    --> git log
+                                --> git log --oneline
+                                The above command gives short commit ID followed by message
+
+*   To check the commit --> git checkout checkout-ID
+*   To delete the specific commit   --->    git revert checkout-ID
+    The above command deletes the commit from the project, but it will be present in the git
+*   To delete the commit permanently    --> git reset checkout-ID
+
+*   To push the code    --> git push repo_url master
+
+*   To clone            --> git clone repo_url
+
+*   To pull             --> git pull
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
